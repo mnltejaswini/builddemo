28d485d1200af974e90ab9b3562704a226d09b39
Tejaswini Duggaraju <naduggar@microsoft.com>
2017-12-05 18:31:04 -0800

    Test commit

--- BuildDemoApp/Program.cs
+++ BuildDemoApp/Program.cs
@@ -6,7 +6,7 @@ namespace BuildDemoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git  World!");
         }
     }
 }
